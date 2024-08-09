2e810f6067d5c738adbc2a4eda21a11bf41317f2
Gan Huang <344031924@qq.com>
2024-08-09 17:53:06 +0800

    Update test_3.py

--- GanHuang/test_3.py
+++ GanHuang/test_3.py
@@ -11,20 +11,27 @@ freqs = np.fft.fftfreq(len(t), t[1] - t[0])
 freq_bin=np.arange(8, 16, 0.4)
 
 corr=np.zeros((40,20))
-cca = CCA(n_components=1)
+cca = CCA(n_components=2)
+data=np.zeros((40,0,500))
 
 for idx_sub in range(3):
     for idx_run in range(5):
-        data = np.load('..\data\data1\X_data_subject_'+str(idx_sub+1)+'.'+str(idx_run+1)+'.npy')
-        print([idx_sub,idx_run])
-        for idx_trial in range(40):
-            X=data[idx_trial,:,:].T
-            for idx_freq in range(20):
-                Y=np.zeros((500,4))
-                Y[:,0] = np.sin(2 * np.pi * freq_bin[idx_freq] * t) 
-                Y[:,1] = np.cos(2 * np.pi * freq_bin[idx_freq] * t) 
-                Y[:,2] = np.sin(2 * np.pi * 2 * freq_bin[idx_freq] * t) 
-                Y[:,3] = np.cos(2 * np.pi * 2 * freq_bin[idx_freq] * t)     
-                cca.fit(X, Y)
-                corr[idx_trial,idx_freq] += cca.score(X, Y)
+        temp = np.load('..\data\data1\X_data_subject_'+str(idx_sub+1)+'.'+str(idx_run+1)+'.npy')
+        data = np.concatenate((data, temp), axis=1)
+
+
+for idx_trial in range(40):
+    for k in range(10):
+        idx_ch=np.random.choice(np.arange(120), size=8, replace=False)
+        X=data[idx_trial,idx_ch,:].T
+        print([idx_trial,k])
+        for idx_freq in range(20):
+            Y=np.zeros((500,4))
+            Y[:,0] = np.sin(2 * np.pi * freq_bin[idx_freq] * t) 
+            Y[:,1] = np.cos(2 * np.pi * freq_bin[idx_freq] * t) 
+            Y[:,2] = np.sin(2 * np.pi * 2 * freq_bin[idx_freq] * t) 
+            Y[:,3] = np.cos(2 * np.pi * 2 * freq_bin[idx_freq] * t)     
+                    
+            cca.fit(X, Y)
+            corr[idx_trial,idx_freq] += cca.score(X, Y)
         
