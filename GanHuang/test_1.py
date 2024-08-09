ab99926fa18526f7621760cb809d7ab9a6748663
Gan Huang <344031924@qq.com>
2024-08-09 17:09:33 +0800

    asdfgsad

--- GanHuang/test_1.py
+++ GanHuang/test_1.py
@@ -1,31 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
-X1 = np.load('..\data\data1\X_data_subject_1.2.npy')
-X2 = np.load('..\data\data1\X_data_subject_2.2.npy')
-X3 = np.load('..\data\data1\X_data_subject_3.2.npy')
-Y1 = np.load('..\data\data1\y_labels_subject_1.2.npy')
-Y2 = np.load('..\data\data1\y_labels_subject_2.2.npy')
-Y3 = np.load('..\data\data1\y_labels_subject_3.2.npy')
 
 
-t = np.arange(0, 1, 0.002)
-# signal = np.sin(2 * np.pi * 10 * t) + 0.5 * np.sin(2 * np.pi * 20 * t)
-fft_result=np.zeros((250,))
-freqs = np.fft.fftfreq(len(t), t[1] - t[0])
-f_idx=10
-for k in range(8):
-    signal=X1[0+f_idx,k,:]
-    fft_result += np.abs(np.fft.fft(signal))[:len(freqs)//2]
-    signal=X1[1+f_idx,k,:]
-    fft_result += np.abs(np.fft.fft(signal))[:len(freqs)//2]
-    signal=X2[0+f_idx,k,:]
-    fft_result += np.abs(np.fft.fft(signal))[:len(freqs)//2]
-    signal=X2[1+f_idx,k,:]
-    fft_result += np.abs(np.fft.fft(signal))[:len(freqs)//2]
-    signal=X3[0+f_idx,k,:]
-    fft_result += np.abs(np.fft.fft(signal))[:len(freqs)//2]
-    signal=X3[1+f_idx,k,:]
-    fft_result += np.abs(np.fft.fft(signal))[:len(freqs)//2]
+for idx_label in range(20):
+    t = np.arange(0, 1, 0.002)
+    fft_result=np.zeros((250,))
+    freqs = np.fft.fftfreq(len(t), t[1] - t[0])
+    for idx_sub in range(3):
+        for idx_run in range(5):
+            X = np.load('..\data\data1\X_data_subject_'+str(idx_sub+1)+'.'+str(idx_run+1)+'.npy')
+            for idx_chn in range(8):
+                signal=X[0+idx_label*2,idx_chn,:]
+                fft_result += np.abs(np.fft.fft(signal))[:len(freqs)//2]
+                signal=X[1+idx_label*2,idx_chn,:]
+                fft_result += np.abs(np.fft.fft(signal))[:len(freqs)//2]
+        
+    plt.plot(freqs[:len(freqs)//2], np.abs(fft_result)[:len(freqs)//2])
+    plt.title('Frequency Spectrum')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Magnitude')
+    plt.xlim(5, 30)
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
 
 
 # # 绘制原始信号
@@ -35,12 +32,3 @@ for k in range(8):
 
 # # 绘制频谱
 # plt.subplot(2, 1, 2)
-
-plt.plot(freqs[:len(freqs)//2], np.abs(fft_result)[:len(freqs)//2])
-plt.title('Frequency Spectrum')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Magnitude')
-plt.xlim(5, 20)
-plt.grid(True)
-plt.tight_layout()
-plt.show()
---
+++ GanHuang/test_2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 17:09:14 2024
+
+@author: Administrator
+"""
+
